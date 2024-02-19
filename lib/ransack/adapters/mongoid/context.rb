16c941f4ed7e74f62df013179ca8324e1d77449a
Diego del Valle <diego.valle@dixtra.co>
2024-02-19 16:17:32 -0300

    fix: activeadmin bug

--- lib/ransack/adapters/mongoid/context.rb
+++ lib/ransack/adapters/mongoid/context.rb
@@ -5,7 +5,6 @@ module Ransack
   module Adapters
     module Mongoid
       class Context < ::Ransack::Context
-
         def initialize(object, options = {})
           super
           # @arel_visitor = @engine.connection.visitor
@@ -17,7 +16,8 @@ module Ransack
 
         def type_for(attr)
           return nil unless attr && attr.valid?
-          name    = attr.arel_attribute.name.to_s.split('.').last
+
+          name = attr.arel_attribute.name.to_s.split('.').last
           # table   = attr.arel_attribute.relation.table_name
 
           # schema_cache = @engine.connection.schema_cache
@@ -39,9 +39,9 @@ module Ransack
           t.to_s.demodulize.underscore.to_sym
         end
 
-        def evaluate(search, opts = {})
+        def evaluate(search, _opts = {})
           viz = Visitor.new
-          relation = @object.where(viz.accept(search.base))
+          relation = @object.where(viz.accept(search.base) || {})
           if search.sorts.any?
             ary_sorting = viz.accept(search.sorts)
             sorting = {}
@@ -65,15 +65,15 @@ module Ransack
             remainder = []
             found_assoc = nil
             while !found_assoc && remainder.unshift(
-              segments.pop) && segments.size > 0 do
+              segments.pop
+            ) && segments.size > 0
               assoc, poly_class = unpolymorphize_association(
                 segments.join('_')
-                )
-              if found_assoc = get_association(assoc, klass)
-                exists = attribute_method?(remainder.join('_'),
-                  poly_class || found_assoc.klass
-                )
-              end
+              )
+              next unless found_assoc = get_association(assoc, klass)
+
+              exists = attribute_method?(remainder.join('_'),
+                                         poly_class || found_assoc.klass)
             end
           end
           exists
@@ -85,7 +85,7 @@ module Ransack
         end
 
         def klassify(obj)
-          if Class === obj && obj.ancestors.include?(::Mongoid::Document)
+          if obj.is_a?(Class) && obj.ancestors.include?(::Mongoid::Document)
             obj
           elsif obj.respond_to? :klass
             obj.klass
@@ -96,15 +96,15 @@ module Ransack
           end
         end
 
-        def lock_association(association)
-          warn "lock_association is not implemented for Ransack mongoid adapter" if $DEBUG
+        def lock_association(_association)
+          warn 'lock_association is not implemented for Ransack mongoid adapter' if $DEBUG
         end
 
-        def remove_association(association)
-          warn "remove_association is not implemented for Ransack mongoid adapter" if $DEBUG
+        def remove_association(_association)
+          warn 'remove_association is not implemented for Ransack mongoid adapter' if $DEBUG
         end
 
-      private
+        private
 
         def get_parent_and_attribute_name(str, parent = @base)
           attr_name = nil
@@ -115,14 +115,15 @@ module Ransack
             remainder = []
             found_assoc = nil
             while remainder.unshift(
-              segments.pop) && segments.size > 0 && !found_assoc do
+              segments.pop
+            ) && segments.size > 0 && !found_assoc
               assoc, klass = unpolymorphize_association(segments.join('_'))
-              if found_assoc = get_association(assoc, parent)
-                parent, attr_name = get_parent_and_attribute_name(
-                  remainder.join('_'), found_assoc.klass
-                  )
-                attr_name = "#{segments.join('_')}.#{attr_name}"
-              end
+              next unless found_assoc = get_association(assoc, parent)
+
+              parent, attr_name = get_parent_and_attribute_name(
+                remainder.join('_'), found_assoc.klass
+              )
+              attr_name = "#{segments.join('_')}.#{attr_name}"
             end
           end
 
@@ -168,11 +169,11 @@ module Ransack
           join_nodes = buckets[Constants::JOIN_NODE] || []
 
           string_joins = (buckets[Constants::STRING_JOIN] || [])
-            .map { |x| x.strip }
-            .uniq
+                         .map { |x| x.strip }
+                         .uniq
 
           join_list = relation.send :custom_join_ast,
-            relation.table.from(relation.table), string_joins
+                                    relation.table.from(relation.table), string_joins
 
           join_dependency = JoinDependency.new(
             relation.klass, association_joins, join_list
@@ -188,24 +189,23 @@ module Ransack
         # ActiveRecord method
         def build_or_find_association(name, parent = @base, klass = nil)
           found_association = @join_dependency.join_associations
-          .detect do |assoc|
+                                              .detect do |assoc|
             assoc.reflection.name == name &&
-            assoc.parent == parent &&
-            (!klass || assoc.reflection.klass == klass)
+              assoc.parent == parent &&
+              (!klass || assoc.reflection.klass == klass)
           end
           unless found_association
             @join_dependency.send(
               :build,
               Polyamorous::Join.new(name, @join_type, klass),
               parent
-             )
+            )
             found_association = @join_dependency.join_associations.last
             # Leverage the stashed association functionality in AR
             @object = @object.joins(found_association)
           end
           found_association
         end
-
       end
     end
   end
